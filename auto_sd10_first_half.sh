648ec87e1785f9fe6cc380c64063375330bdc861
loveloveempress <love@AEWIN1138.aewin.com>
2024-06-03 16:08:33 +0800

    auto user privilege
    + auto_sd10_userlist.sh

---
+++ auto_sd10_first_half.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+### created	:	Mon Jun  3 16:03:35 CST 2024
+### date	:	.
+### auto	:	lovecoding-queen
---
+++ auto_sd10_userlist.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+### created	:	Fri May 31 15:44:13 CST 2024
+### date	:	Mon Jun  3 16:04:54 CST 2024
+### auto	: loveloveempress
+
+global_ip=$(grep ip javascript_ip.js|cut -d ' ' -f 3| cut -d '"' -f 2);
+
+user_list_0=$(ipmitool -I lanplus -H${global_ip} -Uadmin -P11111111 user list)
+# echo -en "love me \r";
+# echo -e "1314 \r";
+printf "${user_list_0}">>./result/user_list_0.txt;
+grep -i 'admin' result/user_list.txt>user_admin_0.txt;
+grep 'false' result/user_list_0.txt
+if (( "$?" == 0 ));then
+    ipmitool -I lanplus -H${global_ip} -Uadmin -P11111111 channel setaccess 1 2 callin=on ipmi=on link=on privilege=4
+    sleep 21;		###Im not sure how many time (but in my case 21s)
+    user_list_1=$(ipmitool -I lanplus -H${global_ip} -Uadmin -P11111111 user list);
+    printf "${user_list_1}">>./result/user_list_1.txt;
+else
+    printf "now all channel open";
+fi
