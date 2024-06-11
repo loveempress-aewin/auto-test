bd7ee28f9bb7d13e87619c4aa0d476dcde7af9e7
loveloveempress <love@AEWIN1138.aewin.com>
2024-06-11 13:14:01 +0800

    fix : fix catch old  --> the old Var catch only one time (not put function)
    fix : fix bug --> monitoring  out condition $V==$A --> $V == $A (big
    error)

--- auto_bbu_watchdog.sh
+++ auto_bbu_watchdog.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 ### created	:	Thu Jun  6 10:37:50 CST 2024
-### date	:	Fri Jun  7 11:35:48 CST 2024
+### date	:	Tue Jun 11 11:46:01 CST 2024
 ### auto	:	lovecoding-queen
 ###
 global_ip=$(grep ip javascript_ip.js|cut -d ' ' -f 3| cut -d '"' -f 2);
 tem_flag=0;
 printf "">./result/watchdog.txt;
+    . ./auto_function_elist.sh 4;
+    local_elist_count_old="${sel_elist_count}";
 function_watchdog_get(){
     if [[ ! -f ./result/watchdog.txt ]];then
         date>>./result/watchdog.txt;
@@ -20,9 +22,7 @@ function_watchdog_get(){
     printf "${watchdog}\n\n">>./result/watchdog.txt
 }
 function_execute(){
-    . ./auto_function_elist.sh 4;
-    local_elist_count_old="${sel_elist_count}";
-    printf "index[0]:-->[${local_elist_count_old}]\n">>./result/watchdog.txt;
+    printf "index[0]:>[${local_elist_count_old}]\n">>./result/watchdog.txt;
     printf "\n========start sel elist========\n${sel_elist}\n========\n\n">>./result/watchdog.txt;
     function_watchdog_get;
     printf "and now we need to set watchdog --by auto test\n">>./result/watchdog.txt;
@@ -35,22 +35,22 @@ function_execute(){
     function_watchdog_get;
     sleep 2;
     function_watchdog_get;
-    ./auto_function_process.sh 10;
+    ./auto_function_process.sh 70;
     printf "================\n">>./result/watchdog.txt;
     . ./auto_function_elist.sh 4;
     local_elist_count_end="${sel_elist_count}";
-    printf "index[1]:[${local_elist_count_end}]\n">>./result/watchdog.txt;
+    printf "index[1]:---->[${local_elist_count_end}]\n">>./result/watchdog.txt
     printf "${sel_elist}\n">>./result/watchdog.txt;
     let tem_flag++;
-    printf "\n\n~~~~~~~~\n old:[${local_elist_count_old}]\nend:[${local_elist_count_end}]\n";
-    printf "\n tem[${tem_flag}]\n";
     function_monitoring;
+    # printf "\ntem : [${tem_flag}]";
+    printf "\n old : [${local_elist_count_old}] --> [${local_elist_count_end}]";
 }
 #### fix bug : if you need to repeat command need to put function...
 function_monitoring(){
-if [[ "${local_elist_count_old}"=="${local_elist_count_end}" ]];then
+if [[ "${local_elist_count_old}" == "${local_elist_count_end}" ]];then
+    printf " Your watchdog did not execute precisely! ";
     if [[ ${tem_flag} == 1 ]];then
-        printf "\n @@@@@@@\n";
         function_execute;
     else
         printf "\n\nERROR!!!!because you catch time to fast!!!\n\n">>./result/watchdog.txt;
@@ -58,7 +58,7 @@ if [[ "${local_elist_count_old}"=="${local_elist_count_end}" ]];then
         ###  not use for dev
         exit 111111;
     fi
-    printf " Your watchdog did not execute precisely! ";
 fi
 }
+
 function_execute;
